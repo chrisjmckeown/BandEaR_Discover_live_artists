b40d888f57b8e99744fc8df9d97b19be2e8b8fad
Chris Mckeown <chris.j.mckeown@hotmail.com>
2020-09-15 19:44:32 +1000

    basic google map functioning

--- Assets/JS/google.js
+++ Assets/JS/google.js
@@ -1,32 +1,31 @@
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8
+  });
+}
+
 $(document).ready(function () {
 
-    getDefaultCityCountry();
+    // getDefaultCityCountry();
 
-    // use ipinfo to source the local city and country
-    function getDefaultCityCountry() {
-        // Create an AJAX call to retrieve data Log the data in console
-        var queryParameters = {
-            token: ipinfoAPIKey,
-        };
-        var queryString = $.param(queryParameters);
-        var queryURL = "https://ipinfo.io?" + queryString;
-        // Call with a get method
-        $.ajax({ url: queryURL, method: 'get' }).then(function (response) {
-            defaultCity = response.city + ", " + response.country;
-        }).catch(function (err) {
-            console.log(err);
-        }).always(function () {
-            loadPage(); // this will run even if response fails
-        });
-    };
-
-    
-    let map;
-
-    function initMap() {
-      map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 8
-      });
-    }
+    // // use ipinfo to source the local city and country
+    // function getDefaultCityCountry() {
+    //     // Create an AJAX call to retrieve data Log the data in console
+    //     var queryParameters = {
+    //         token: ipinfoAPIKey,
+    //     };
+    //     var queryString = $.param(queryParameters);
+    //     var queryURL = "https://ipinfo.io?" + queryString;
+    //     // Call with a get method
+    //     $.ajax({ url: queryURL, method: 'get' }).then(function (response) {
+    //         defaultCity = response.city + ", " + response.country;
+    //     }).catch(function (err) {
+    //         console.log(err);
+    //     }).always(function () {
+    //         loadPage(); // this will run even if response fails
+    //     });
+    // };
 });
