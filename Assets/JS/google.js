e7b64145f58c9003ecdbd18c75ab09342988ac54
Chris Mckeown <chris.j.mckeown@hotmail.com>
2020-09-15 19:23:43 +1000

    git ignore added

--- Assets/JS/google.js
+++ Assets/JS/google.js
@@ -19,4 +19,14 @@ $(document).ready(function () {
             loadPage(); // this will run even if response fails
         });
     };
+
+    
+    let map;
+
+    function initMap() {
+      map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8
+      });
+    }
 });
