1c87b1ca2c9d2882062d52d10381c32f5aa8be93
Chris Mckeown <chris.j.mckeown@hotmail.com>
2020-09-15 19:09:29 +1000

    document ready function added

---
+++ Assets/JS/google.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+
+    getDefaultCityCountry();
+
+    // use ipinfo to source the local city and country
+    function getDefaultCityCountry() {
+        // Create an AJAX call to retrieve data Log the data in console
+        var queryParameters = {
+            token: ipinfoAPIKey,
+        };
+        var queryString = $.param(queryParameters);
+        var queryURL = "https://ipinfo.io?" + queryString;
+        // Call with a get method
+        $.ajax({ url: queryURL, method: 'get' }).then(function (response) {
+            defaultCity = response.city + ", " + response.country;
+        }).catch(function (err) {
+            console.log(err);
+        }).always(function () {
+            loadPage(); // this will run even if response fails
+        });
+    };
+});
