e28e1ba80c6b2b925741d152d20ee341fc699d54
Simone Cheah <simone_cheah@hotmail.com>
2020-09-15 21:24:11 +1000

    added bandsintown api functionality for artist and event information

---
+++ Assets/JS/bandsintown.js
@@ -0,0 +1,59 @@
+$("#searchBtn").on("click", function() {
+    var artist = $("#artist-input").val().trim();
+
+    if (artist) {
+        var artistURL = "https://rest.bandsintown.com/artists/" + artist + "?app_id=codingbootcamp";
+
+        $.ajax({
+            url: artistURL,
+            method: "GET"
+        }).then(function(response) {
+            // sleeping with sirens
+            console.log(response);
+
+            $("#artist-name").text(response.name);
+            $("#artist-image").attr("src", response.thumb_url); // artist image
+            $("#band-details").append("<br>" + response.upcoming_event_count + " upcoming events");
+
+            var events = response.upcoming_event_count
+            if (events > 0) {
+                var eventsURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+
+                $.ajax({
+                    url: eventsURL,
+                    method: "GET"
+                }).then(function(response) {
+                    console.log("events:")
+                    var data = response[0]
+                    console.log(data)
+
+                    // venue
+                    console.log(data.venue.type)
+                    if (data.venue.name) {
+                        $("#band-details").append("<br>" + data.venue.name)
+                    }
+                    if (data.title) {
+                        $("#band-details").append("<br>" + data.title)
+                    }
+                    if (data.venue.city) {
+                        $("#band-details").append("<br>" + data.venue.city)
+                        if (data.venue.location) {
+                            $("#band-details").append(", " + data.venue.location)
+                        }
+                    }
+                    
+                    if (data.url) {
+                        var br = $("<br>");
+                        var aTag = $("<a>").text("ticket");
+                        aTag.attr("href", data.offers[0].url)
+                        $("#band-details").append(br)
+                        $("#band-details").append(aTag)
+
+                        console.log(data.offers[0].url)
+                    }
+                })
+            }
+            
+        });
+    }
+})
