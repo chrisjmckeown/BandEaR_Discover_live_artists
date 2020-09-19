ef736677a87bb26edce7d644effe4dc40e3a42aa
Chris Mckeown <chris.j.mckeown@hotmail.com>
2020-09-19 17:58:45 +1000

    Adjusting js to work with new upcoming events div

--- Assets/JS/bandsintown.js
+++ Assets/JS/bandsintown.js
@@ -1,5 +1,31 @@
 $(document).ready(function () {
     //#region spotify
+    setMainBody(false);
+
+    function setMainBody(searchInProgress) {
+        if (!searchInProgress) {
+            // set the divs
+            // Set middle section
+            $("#blurb-about-site").attr("style", "height: 82vh; display: block");
+            $("#bands-in-town").attr("style", "display: none");
+            // Set right section
+            $("#map-canvas").attr("style", "height: 82vh; display: block");
+            $("#event-information").attr("style", "display: none");
+            $('#band-details').empty()
+            getDefaultCityCountry();
+        }
+        else {
+            $("#bands-in-town-list").empty();
+            $("#event-information-content").empty();
+            // Set middle section
+            $("#blurb-about-site").attr("style", "display: none");
+            $("#bands-in-town").attr("style", "height: 82vh; display: block");
+            // Set right section
+            $("#map-canvas").attr("style", "height: 40vh; display: block");
+            $("#event-information").attr("style", "height: 40vh; display: block");
+            getDefaultCityCountry();
+        }
+    }
 
     let artists_search_results = []
     let artist_id = []
@@ -44,34 +70,33 @@ $(document).ready(function () {
     }
 
     $('#search-form').submit(function (event) {
-        event.preventDefault()
-        get_results()
+        event.preventDefault();
+        get_results();
     })
+
+    $("#artist-input").keyup(function () {
+        var input = $(this).val().trim();
+        if (!input) {
+            setMainBody(false);
+        }
+    });
     //#endregion
 
     //#region bandsintown
+
     $(document).on('click', '#spotify', function (event) { event.stopPropagation() })
 
     $(document).on('click', '.artist', function (event) {
-        // set the divs
-        $("#map-canvas").attr("style", "display: none");
 
-        $("#blurb-about-site").attr("style", "display: none");
-        $("#bands-in-town").attr("style", "height: 82vh; display: block");
- 
-        $("#map-canvas").attr("style", "height: 40vh; display: block");
-        $("#event-information").attr("style", "height: 40vh; display: block");
-        // , $(this).attr("title") + "Appended text.");
-
+        setMainBody(true);
         let artistId = artist_id[parseInt(this.id)]
         let divId = this.id
         // let artist = artists_search_results[parseInt(this.id)]
         $.get({
             url: `https://api.spotify.com/v1/artists/${artistId}/top-tracks?country=AU`,
             headers: { 'Authorization': 'Bearer ' + token }
-        }).then(function (dat) {
-            console.log(dat);
-            const tracks = dat.tracks
+        }).then(function (response) {
+            const tracks = response.tracks
             const new_Div = $(`<div id='spotify' class='display_hits'><p class='text-right'>x</p><h5>Top Hits</h5> </div>`)
             $(`#${divId}`).append(new_Div)
             $('.display_hits').show()
@@ -94,14 +119,21 @@ $(document).ready(function () {
         });
     })
 
-    function appendArtistInfo(data) {
+    function appendArtistInfo(artist, data) {
         // artist info
+        // check and set the artist name and add to card title -> bands-in-town-band-name
         if (data.name) {
-            $("#bands-in-town-list").append($("<h2>").text(data.name).attr("style", "margin: 0 0 5px 0"));
+            $("#bands-in-town-band-name").text(data.name);
         }
+        // check and set the upcoming event count, if none then display no upcoming events
         if (data.upcoming_event_count) {
-            $("#bands-in-town-list").append($("<p>").text(data.upcoming_event_count + " upcoming events"));
-        } else { $("#bands-in-town-list").append($("<p>").text(artist + " is not in town")) };
+            $("#event-information-content").append($("<p>").text(data.upcoming_event_count + " upcoming events."));
+        }
+        else {
+            $("#event-information-content").append($("<p>").text(artist + " is not in town."))
+            // reset map to your location
+            initMap(defaultCoodinates);
+        };
         if (data.thumb_url) {
             $("#bands-in-town-list").append($("<img>").attr("src", data.image_url).width("100%"));
         }
@@ -109,23 +141,34 @@ $(document).ready(function () {
 
     function appendEventInfo(data) {
         //events
-        $("#bands-in-town-list").append("Next upcoming event: ")
+        $("#event-information-content").append("Next upcoming event: ")
 
         // venue info
         if (data.venue.name) {
-            $("#bands-in-town-list").append($("<h6>").text(data.venue.name))
+            $("#event-information-content").append($("<h6>").text(data.venue.name))
         }
         if (data.title) {
-            $("#bands-in-town-list").append($("<h5>").text(data.title).attr("style", "margin-top: 0"))
+            $("#event-information-content").append($("<h5>").text(data.title).attr("style", "margin-top: 0"))
         }
         if (data.venue.location) {
-            $("#bands-in-town-list").append("Location: " + data.venue.location)
-        } else { $("#bands-in-town-list").append("Location: " + data.venue.type) }
+            $("#event-information-content").append("Location: " + data.venue.location)
+        }
+        else {
+            $("#event-information-content").append("Location: " + data.venue.type)
+        }
 
         // ticket info
         if (data.offers[0].url) {
-            var ticketInfo = $("<a>").text("Tickets available here").attr("href", data.offers[0].url).attr("target", "_blank")
-            $("#bands-in-town-list").append($("<p>").append(ticketInfo))
+            $("#event-information-content").append('<p><a href=' + data.offers[0].url + ' target="_blank">Tickets available here</a></p>')
+        }
+
+        // lat, long
+        if (data.venue.latitude && data.venue.longitude) {
+            var coordinates = {
+                latitude: parseFloat(data.venue.latitude),
+                longitude: parseFloat(data.venue.longitude)
+            };
+            initMap(coordinates)
         }
     }
 
@@ -139,9 +182,6 @@ $(document).ready(function () {
                 url: artistURL,
                 method: "GET"
             }).then(function (response) {
-                $("#blurb-about-site").attr("style", "display: none")
-                $("#bands-in-town-list").empty();
-
                 // error checking
                 if (response.error) {
                     $("#bands-in-town-list").append(
@@ -151,7 +191,9 @@ $(document).ready(function () {
                     $("#bands-in-town-list").append(
                         ($("<h2>").text(artist).attr("style", "margin: 0 0 5px 0")),
                         ($("<p>").text(artist + " is not in town")))
-                } else { appendArtistInfo(response); }
+                } else { 
+                    appendArtistInfo(artist, response); 
+                }
 
                 if (response.upcoming_event_count > 0) {
                     var eventURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
@@ -162,15 +204,6 @@ $(document).ready(function () {
                     }).then(function (response) {
                         var data = response[0]
                         appendEventInfo(data)
-
-                        // lat, long
-                        if (data.venue.latitude && data.venue.longitude) {
-                            var coordinates = {
-                                latitude: parseFloat(data.venue.latitude),
-                                longitude: parseFloat(data.venue.longitude)
-                            };
-                            initMap(coordinates)
-                        }
                     })
                 }
             })
@@ -198,18 +231,18 @@ $(document).ready(function () {
         };
         // set the map variable, center location, and zoom
         map = new google.maps.Map($('#map-canvas')[0], mapOptions);
-        var image = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
+        // var image = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
 
-        // marker = new google.maps.Marker({
-        //     position: { lat: coordinates.latitude, lng: coordinates.longitude },
-        //     title: 'Hello World!',
-        //     animation: google.maps.Animation.DROP,
-        //     icon: image,
-        // });
+        marker = new google.maps.Marker({
+            position: { lat: coordinates.latitude, lng: coordinates.longitude },
+            // title: 'Hello World!',
+            animation: google.maps.Animation.DROP,
+            // icon: image,
+        });
 
-        //marker.setMap(map);
-        //marker.addListener('click', toggleBounce);
-        // marker.setMap(null); // remove markers
+        marker.setMap(map);
+        marker.addListener('click', toggleBounce);
+        //marker.setMap(null); // remove markers
     }
 
     // function initMap() {
@@ -223,55 +256,55 @@ $(document).ready(function () {
 
     // Data for the markers consisting of a name, a LatLng and a zIndex for the
     // order in which these markers should display on top of each other.
-    var beaches = [
-        ['Bondi Beach', -33.890542, 151.274856, 4],
-        ['Coogee Beach', -33.923036, 151.259052, 5],
-        ['Cronulla Beach', -34.028249, 151.157507, 3],
-        ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
-        ['Maroubra Beach', -33.950198, 151.259302, 1]
-    ];
+    // var beaches = [
+    //     ['Bondi Beach', -33.890542, 151.274856, 4],
+    //     ['Coogee Beach', -33.923036, 151.259052, 5],
+    //     ['Cronulla Beach', -34.028249, 151.157507, 3],
+    //     ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
+    //     ['Maroubra Beach', -33.950198, 151.259302, 1]
+    // ];
 
-    function setMarkers(map) {
-        // Adds markers to the map.
+    // function setMarkers(map) {
+    //     // Adds markers to the map.
 
-        // Marker sizes are expressed as a Size of X,Y where the origin of the image
-        // (0,0) is located in the top left of the image.
+    //     // Marker sizes are expressed as a Size of X,Y where the origin of the image
+    //     // (0,0) is located in the top left of the image.
 
-        // Origins, anchor positions and coordinates of the marker increase in the X
-        // direction to the right and in the Y direction down.
-        var image = {
-            url: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-            // This marker is 20 pixels wide by 32 pixels high.
-            size: new google.maps.Size(20, 32),
-            // The origin for this image is (0, 0).
-            origin: new google.maps.Point(0, 0),
-            // The anchor for this image is the base of the flagpole at (0, 32).
-            anchor: new google.maps.Point(0, 32)
-        };
-        // Shapes define the clickable region of the icon. The type defines an HTML
-        // <area> element 'poly' which traces out a polygon as a series of X,Y points.
-        // The final coordinate closes the poly by connecting to the first coordinate.
-        var shape = {
-            coords: [1, 1, 1, 20, 18, 20, 18, 1],
-            type: 'poly'
-        };
-        for (var i = 0; i < beaches.length; i++) {
-            var beach = beaches[i];
-            marker = new google.maps.Marker({
-                position: {
-                    lat: beach[1],
-                    lng: beach[2]
-                },
-                map: map,
-                icon: image,
-                shape: shape,
-                animation: google.maps.Animation.DROP,
-                title: beach[0],
-                zIndex: beach[3]
-            });
-            marker.addListener('click', toggleBounce);
-        }
-    }
+    //     // Origins, anchor positions and coordinates of the marker increase in the X
+    //     // direction to the right and in the Y direction down.
+    //     var image = {
+    //         url: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+    //         // This marker is 20 pixels wide by 32 pixels high.
+    //         size: new google.maps.Size(20, 32),
+    //         // The origin for this image is (0, 0).
+    //         origin: new google.maps.Point(0, 0),
+    //         // The anchor for this image is the base of the flagpole at (0, 32).
+    //         anchor: new google.maps.Point(0, 32)
+    //     };
+    //     // Shapes define the clickable region of the icon. The type defines an HTML
+    //     // <area> element 'poly' which traces out a polygon as a series of X,Y points.
+    //     // The final coordinate closes the poly by connecting to the first coordinate.
+    //     var shape = {
+    //         coords: [1, 1, 1, 20, 18, 20, 18, 1],
+    //         type: 'poly'
+    //     };
+    //     for (var i = 0; i < beaches.length; i++) {
+    //         var beach = beaches[i];
+    //         marker = new google.maps.Marker({
+    //             position: {
+    //                 lat: beach[1],
+    //                 lng: beach[2]
+    //             },
+    //             map: map,
+    //             icon: image,
+    //             shape: shape,
+    //             animation: google.maps.Animation.DROP,
+    //             title: beach[0],
+    //             zIndex: beach[3]
+    //         });
+    //         marker.addListener('click', toggleBounce);
+    //     }
+    // }
 
     function toggleBounce() {
         if (marker.getAnimation() !== null) {
@@ -282,9 +315,9 @@ $(document).ready(function () {
     }
 
     // ipinfo API key
+    var defaultCoodinates;
     var ipinfoAPIKey = "f2357f3657a5f4";
     getDefaultCityCountry();
-
     // use ipinfo to source the local city and country
     function getDefaultCityCountry() {
         var coordinates = getStoredCoordinates();
@@ -300,17 +333,21 @@ $(document).ready(function () {
             method: 'get'
         }).then(function (response) {
             // split the location string to parse the longitude and latitude
-            var loc = response.loc.split(',');
-            coordinates = {
-                latitude: parseFloat(loc[0]),
-                longitude: parseFloat(loc[1])
-            };
-            // initialise the map
-            initMap(coordinates);
-            setStoredCoordinates(coordinates);
+            if (response.loc) {
+                var loc = response.loc.split(',');
+                coordinates = {
+                    latitude: parseFloat(loc[0]),
+                    longitude: parseFloat(loc[1])
+                };
+
+                // initialise the map
+                initMap(coordinates);
+                setStoredCoordinates(coordinates);
+            }
         }).catch(function (err) {
-            console.log(err);
+             console.log(err);
         });
+        defaultCoodinates = coordinates;
     };
 
     // Get from local storage
